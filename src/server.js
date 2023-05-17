7c159e75cb5280207d2c9ba7fda956c8278096a3
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-05-17 15:30:10 -0500

    starting

---
+++ src/server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express());
+
+app.get('/', (req, res, next) => {
+  res.
+})
