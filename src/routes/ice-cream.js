f0eda67cb762399ae8831fc767affae242219e9d
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-05-17 22:20:12 -0500

    nothing

--- src/routes/ice-cream.js
+++ src/routes/ice-cream.js
@@ -5,23 +5,18 @@ const router = express.Router();
 const { iceCreamModel } = require('../models/index');
 
 router.get('/ice-cream', async (req, res) => {
-  try {
-    let allIceCream = await iceCreamModel.findAll();
-    
-    res.status(200).send(allIceCream);
-  } catch (error) {
-    next(error);
-  }
+  let allIceCream = await iceCreamModel.findAll();
 
+  res.status(200).send(allIceCream);
 });
 
 router.get('/ice-cream/:id', async (req, res) => {
-  let singleIceCream = await iceCreamModel.findAll({where: {id: req.params.id}});
+  let singleIceCream = await iceCreamModel.findAll({ where: { id: req.params.id } });
 
   res.status(200).send(singleIceCream);
 });
 
-router.post('/ice-cream', async (req, res) => {
+router.post('/ice-cream', async (req, res, next) => {
   let newIceCream = await iceCreamModel.create(req.body);
 
   res.status(200).send(newIceCream);
